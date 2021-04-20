596a7883f8a547aac878cedcd301599b7aa3d0b1
alexis <alexisdrouhincohen@gmail.com>
2021-04-20 15:06:41 +0200

    ceci est un avancement

--- src/projetCours/Visite.java
+++ src/projetCours/Visite.java
@@ -4,15 +4,55 @@ public class Visite {
 
 	String nomEtudiant;
 	String date;
-	int note;
+	Integer note;
 	
-	public Visite() {}
+	String getnomEtudiant() {
+		return this.nomEtudiant;
+	}
 	
-	public Visite(String nomEtudiant,String date,int note) {
+	String getdate() {
+		return this.date;
+	}
+	
+	int getnote() {
+		return this.note;
+	}
+	
+	void setNomEtudiant(String nomEtudiant) {
+		this.nomEtudiant = nomEtudiant;
+	}
+	
+	void setDate(String date) {
+		this.date = date;
+	}
+	
+	void setNote(Integer note) {
+		this.note = note;
+	}
+	
+	public Visite() {
+		this.nomEtudiant=new String();
+		this.date=new String();
+	}
+	
+	public Visite(String nomEtudiant,String date,Integer note) {
 		
 		this.nomEtudiant = nomEtudiant;
 		this.date = date;
 		this.note = note;
+
+	}
+	
+public static void main(String[] args) {
+		Visite visiteV1 = new Visite();
+		System.out.println(visiteV1);
+		visiteV1.note = 5;
+		Visite visiteV2 = new Visite("alexis","03/07/2000",15);
+		System.out.println(visiteV2.getnomEtudiant());
+		System.out.println(visiteV2.getdate());
+		System.out.println(visiteV2.getnote());
+		
+		
 	}
 }
 
