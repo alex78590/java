2ac93e01265cba518be8413035d779ef58355f8d
alexis <alexisdrouhincohen@gmail.com>
2021-04-20 10:48:35 +0200

    ajout de la class visite

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module projetCours {
+}
---
+++ src/projetCours/Visite.java
@@ -0,0 +1,18 @@
+package projetCours;
+
+public class Visite {
+
+	String nomEtudiant;
+	String date;
+	int note;
+	
+	public Visite() {}
+	
+	public Visite(String nomEtudiant,String date,int note) {
+		
+		this.nomEtudiant = nomEtudiant;
+		this.date = date;
+		this.note = note;
+	}
+}
+
